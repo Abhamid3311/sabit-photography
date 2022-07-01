1663a636a17bb40b326a8ffac96b1d83176d7759
Hamid <www.abdulhamid3311@gmail.com>
2022-07-01 11:29:26 +0600

    upgrade banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,70 +1,17 @@
-import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
-import banner1 from '../../images/banner-1.png';
-import banner2 from '../../images/banner-2.jpg';
-import banner3 from '../../images/banner-3.png';
-import banner4 from '../../images/banner-4.jpg';
+import './Banner.css'
+
 
 const Banner = () => {
-    const [index, setIndex] = useState(0);
-
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
-
     return (
-        <div id='banner'>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner1}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <h3>Squirrels </h3>
-                        <p className='text-primary'>He is finding his Companion. motion capture</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner2}
-                        alt="Second slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Chilling Seal</h3>
-                        <p>Enjoying Summer by swimming</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner3}
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Enligted Grasshopper</h3>
-                        <p>Making world more colorful by his own color</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={banner4}
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <h3>Scared Rabbit</h3>
-                        <p>
-                            Time to go for own Life!
-                        </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+        <div id='banner' >
+            <div className='banner-head text-center mx-auto '>
+                <p className='fs-4 fw-bolder '>Welcome to</p>
+                <h1 className='text-primary fs-1 text-uppercase fw-bold '>Sabit Photography </h1>
+                <p>
+                   Photography is the art of black-white what is made by Camera. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid perspiciatis expedita debitis ipsum ut distinctio!
+                </p>
+                <button className='btn btn-outline-primary'>Explorer more</button>
+            </div>
         </div>
     );
 };
